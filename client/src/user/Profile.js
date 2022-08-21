4fe18145b86cf82079b6aaefaeacfbd369eb6746
lvuk <luka.vuk456@gmail.com>
2022-08-21 18:21:07 +0200

    Fetch user data

--- client/src/user/Profile.js
+++ client/src/user/Profile.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import { isAuthenticated } from '../auth';
+import { read } from './apiUser';
 
 class Profile extends Component {
   constructor() {
@@ -10,14 +12,34 @@ class Profile extends Component {
     };
   }
 
-  componentDidMount() {}
+  init = (userId) => {
+    const token = isAuthenticated().token;
+    read(userId, token).then((data) => {
+      if (data.error) {
+        this.setState({ redirectToSignin: true });
+      } else {
+        this.setState({ user: data });
+      }
+    });
+  };
+
+  componentDidMount() {
+    const userId = this.props.match.params.userId;
+    this.init(userId);
+  }
 
   render() {
+    const redirectToSignin = this.state.redirectToSignin;
+    if (redirectToSignin) {
+      return <Redirect to='/signin' />;
+    }
+
     return (
       <div className='container'>
         <h2 className='mt-5 mb-5'>Profile</h2>
         <p>Hello {isAuthenticated().user.username}</p>
         <p>Email {isAuthenticated().user.email}</p>
+        <p>{`Joined ${new Date(this.state.user.created).toDateString()}`}</p>
       </div>
     );
   }
